9840c16d94cba36198bdafbe02e01aba992714fe
Khushi Jain <kj5369227@gmail.com>
2025-11-17 17:14:19 +0530

    env file

--- server/config/db.js
+++ server/config/db.js
@@ -1,24 +1,25 @@
-
-require("dotenv").config();
 const mysql = require("mysql2");
-
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
-
 
 db.connect((err) => {
   if (err) {
     console.error("❌ MySQL connection error:", err);
-  } else {
-    console.log("✅ MySQL Connected!");
+    return;
   }
+  console.log("✅ Connected to Aiven MySQL");
 });
 
 module.exports = db;
+
 
 
