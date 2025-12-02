9d46c68f9e3be477a684c04b9d7eb684742ede16
Khushi-jain05 <kj5369227@gmail.com>
2025-12-02 20:31:09 +0530

    .

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000",'https://splitr-3b3y.vercel.app/'],
+    origin: ["http://localhost:3000",'https://splitr-3b3y.vercel.app'],
     credentials: true,
   })
 );
